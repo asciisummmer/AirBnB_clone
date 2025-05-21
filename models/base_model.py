722d0fa3496e95c484960d14a6476e60e7d9152a
asciisummer <romelsoviguidi@gmail.com>
2025-05-22 00:06:26 +0100

    feat: add base model

--- models/base_model.py
+++ models/base_model.py
@@ -3,15 +3,29 @@
 """ Base model implementation
 """
 import uuid
-import datetime
+from datetime import datetime
+
 
 class BaseModel:
 
     def __init__(self, ):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now() 
+        self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "self.__class__.__name__"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        format_datetime_encoding = "%Y-%m-%dT%H:%M:%S.%f"
+        self.__dict__["__class__"] = self.__class__.__name__
+        self.__dict__["created_at"] = self.created_at.strftime(
+            format_datetime_encoding
+            )
+        self.__dict__["updated_at"] = self.updated_at.strftime(
+            format_datetime_encoding
+            )
+        return self.__dict__
