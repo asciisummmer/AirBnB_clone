887b4eab780e6bf7bc38520514bd237787169c73
asciisummer <romelsoviguidi@gmail.com>
2025-05-22 00:23:31 +0100

    fix: model method to save and init

--- models/base_model.py
+++ models/base_model.py
@@ -8,21 +8,16 @@ from datetime import datetime
 
 class BaseModel:
 
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
-
-    def __init__(self,):
-        self.id = BaseModel.id
-        self.created_at = BaseModel.created_at
-        self.updated_at = BaseModel.updated_at
+    def __init__(self, ):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        BaseModel.updated_at = datetime.now()
-        self.updated_at = BaseModel.updated_at
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         format_datetime_encoding = "%Y-%m-%dT%H:%M:%S.%f"
