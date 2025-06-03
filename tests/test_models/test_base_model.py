6febb233dfd1d0ff3ce9740a41813d1fd766e7fd
asciisummer <romelsoviguidi@gmail.com>
2025-06-04 00:47:59 +0100

    fix coding style

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,20 +6,19 @@ class TestBaseModel(unittest.TestCase):
 
     def setUp(self):
         self.base = BaseModel()
-#        self.base_two = BaseModel()
-
 
     def test__str__(self):
-        self.assertEqual(self.base.__str__(), f"[BaseModel] ({self.base.id}) {self.base.__dict__}")
+        self.assertEqual(
+            self.base.__str__(),
+            f"[BaseModel] ({self.base.id}) {self.base.__dict__}")
 
     def test_created_at(self):
-        self.assertEqual(self.base.created_at, self.base.created_at) 
+        self.assertEqual(self.base.created_at, self.base.created_at)
 
     def test_to_save(self):
         updated_at = self.base.updated_at
-        self.base.save() 
+        self.base.save()
         self.assertNotEqual(self.base.updated_at, updated_at)
-
 
     def test_to_dict(self):
         created_at = self.base.created_at
