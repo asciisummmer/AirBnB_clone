8fe0091fcae76e98a945700e6e7bfa5115eb0e9b
asciisummer <romelsoviguidi@gmail.com>
2025-06-04 00:19:42 +0100

    feat: add unitest for save method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,5 +12,11 @@ class TestBaseModel(unittest.TestCase):
     def test__str__(self):
         self.assertEqual(self.base.__str__(), f"[BaseModel] ({self.base.id}) {self.base.__dict__}")
 
-    def test__created_at(self):
+    def test_created_at(self):
         self.assertEqual(self.base.created_at, self.base.created_at) 
+
+    def test_to_save(self):
+        updated_at = self.base.updated_at
+        self.base.save() 
+        self.assertNotEqual(self.base.updated_at, updated_at)
+        
