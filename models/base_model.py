dfc7c6fe9bc3f5bd446eda9ba55ad9dc55fd5fbc
asciisummer <romelsoviguidi@gmail.com>
2025-06-19 23:31:35 +0100

    feat: create basemodel from kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,28 @@ format_datetime_encoding = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if bool(kwargs):
+            list_keys = kwargs.keys()
+            for key in list_keys:
+                if "created_at" == key:
+                    self.created_at = datetime.strptime(
+                        kwargs["created_at"],
+                        format_datetime_encoding
+                        )
+                elif "updated_at" == key:
+                    self.updated_at = datetime.strptime(
+                        kwargs["updated_at"],
+                        format_datetime_encoding
+                        )
+                elif "__class__" == key:
+                    continue
+                else:
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
